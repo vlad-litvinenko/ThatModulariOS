59a0a9172eddc8a32260e683d01d94828bbe6fd7
Vlad Lytvynenko <vlly@gmx.de>
2020-08-03 18:46:42 +0200

    Add Part 1

---
+++ Part1/ThatClient/ThatClient/SwiftUI/ClientView.swift
@@ -0,0 +1,33 @@
+//
+//  ClientView.swift
+//  ThatClient
+//
+//  Created by Vladyslav Lytvynenko on 25.07.20.
+//  Copyright © 2020 Vladyslav Lytvynenko. All rights reserved.
+//
+
+import SwiftUI
+
+public struct ClientView: View {
+    let service: Service
+    
+    public init(service: Service) {
+        self.service = service
+    }
+    
+    public var body: some View {
+        Text(service.text())
+    }
+}
+
+struct ClientView_Previews: PreviewProvider {
+    static var previews: some View {
+        ClientView(service: DummyService())
+    }
+}
+
+class DummyService: Service {
+    func text() -> String {
+        return "Text from DummyService."
+    }
+}
---
+++ Part1/ThatService/ThatService/ThatService.swift
@@ -0,0 +1,15 @@
+//
+//  ThatService.swift
+//  ThatService
+//
+//  Created by Vladyslav Lytvynenko on 19.07.20.
+//  Copyright © 2020 Vladyslav Lytvynenko. All rights reserved.
+//
+
+public class ThatService {
+    public init() {}
+    
+    public func text() -> String {
+        return "Text from ThatService."
+    }
+}
